3b30bd18c0303ef2e15ef262c4cc2a9bf635880c
Milon0035 <mrmilon075@gmail.com>
2025-06-09 22:52:50 +0600

    added an array called mycars

--- hello.js
+++ hello.js
@@ -2,3 +2,7 @@ function myfunction(name){
     console.log("hello", name);
 }
 myfunction("Milon");
+const mycars=["volvo","ford","toyota"];
+for(i=0;i<mycars.length;i++){
+    console.log(mycars[i]);
+}
