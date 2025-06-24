2a7fcc98b9efd4e4f3f0f1930b403fc049acc699
Milon0035 <mrmilon075@gmail.com>
2025-06-24 16:54:43 +0600

    i will change my js file

--- hello.js
+++ hello.js
@@ -3,14 +3,7 @@ function myfunction(name){
 }
 myfunction("Milon");
 
-let myarray=["apple","mango","banana"];
-for(let v of myarray){
-    console.log(v);
-}
-const mycars=["volvo","ford","toyota"];
-for(i=0;i<mycars.length;i++){
-    console.log(mycars[i]);
-}
+
 const person={
     name:"Md.moklasur Rahman",
     age:25,
